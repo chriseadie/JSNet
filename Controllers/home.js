7ee4fa0b209586aa30a24e631b23cca0e3df666c
chriseadie <caeadie@hotmail.com>
2020-06-17 19:18:33 +0100

    update

--- Controllers/home.js
+++
@@ -1,8 +0,0 @@
-const { View } = require("../core");
-class HomeController {
-    index() {
-        const model = { foo: "bar stool", arr: [{ name: "steve" }, { name: "Jay" }] };
-        return View("index.html", model)
-    }
-}
-module.exports = HomeController;
--- JSNet/Configure.js
+++
@@ -1,9 +0,0 @@
-class Configure {
-    addSingleton(key, value) {
-        global[key] = new value();
-    }
-    destroySingleton(key) {
-        delete global[key];
-    }
-}
-module.exports = Configure;
