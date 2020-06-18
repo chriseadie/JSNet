b7d51e5c83b6934c70dd399335867621c10a36fc
chriseadie <caeadie@hotmail.com>
2020-06-18 21:51:37 +0100

    adding cli to project for easy install

--- JSNet.Server/readme.md
+++ JSNet.Server/readme.md
@@ -9,3 +9,7 @@ Nunjucks has been included as the only dependency thus far to allow templating o
 
 
 I am planing to turn this into a cli to make it much easer to manage and get started. creating action for initiating a project as well as adding controller or views
+
+
+implementing node cli for easy install following this tutorial
+[How to build cli with node js](https://www.twilio.com/blog/how-to-build-a-cli-with-node-js)
---
+++ jsnet-cli/src/cli.js
@@ -0,0 +1,51 @@
+import arg from 'arg';
+import inquirer from 'inquirer';
+
+function parseArgumentsIntoOptions(rawArgs) {
+    const args = arg({
+        "--yes": Boolean,
+        "--install": Boolean,
+        "-i": "--install",
+        "-y": "--yes"
+    }, {
+        argv: rawArgs.slice(2)
+    })
+    return {
+        skipPrompts: args['--yes'] || false,
+        template: args._[0],
+        runInstall: args['--install'] || false
+    }
+}
+
+async function promptForMissingOptions(options) {
+    const defaultTemplate = "Javascript";
+    if (options.skipPrompts) {
+        return {
+            ...options,
+            template: options.template || defaultTemplate
+        }
+    }
+
+    const questions = new Array();
+    if (!options.template) {
+        questions.push({
+            type: "list",
+            name: "template",
+            message: 'Please choose which server to use',
+            choices: ["Javascript"],
+            default: defaultTemplate
+        });
+    }
+
+    const answers = await inquirer.prompt(questions);
+    return {
+        ...options,
+        template: options.template || answers.template
+    };
+}
+
+export async function cli(args) {
+    let options = parseArgumentsIntoOptions(args);
+    options = await promptForMissingOptions(options);
+    console.log(options)
+}
