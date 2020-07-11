efdad16b031824baa4d629e0155df560d0d76964
chriseadie <caeadie@hotmail.com>
2020-07-11 16:44:13 +0100

    fixed issue with api project where header was being set incorrectly

--- JSNet.Api/Javascript/index.js
+++ JSNet.Api/Javascript/index.js
@@ -5,7 +5,7 @@ function ConfigureServices() {
 }
 function Configure() {
     http.createServer((request, response) => {
-        response.writeHead({ "Access-Control-Allow-Origin": "*" })
+        response.setHeader("Access-Control-Allow-Origin", "*")
         setDefaultHeaders(response);
         routerSwitch(request, response);
     }).listen(8080)
